d23f74c404681fe8f6f798ce1028902a2e334ff0
rintan-r <dev.rintanrenatta@gmail.com>
2025-04-12 00:30:13 -0400

    The update was messy, but it's been improved and now works properly

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -9,21 +9,22 @@ class ContentRating extends Component {
         likes : 0,
         dislikes : 0,
         totalRatings : 0,
-
-        handleLike: () => {
-            this.setState((prevState) => ({
-              likes: prevState.likes + 1,
-              totalRatings: prevState.totalRatings + 1
-            }));
-          },
-          handleDislike: () => {
-            this.setState((prevState) => ({
-              dislikes: prevState.dislikes + 1,
-              totalRatings: prevState.totalRatings + 1
-            }));
-          }
     }
+  };
+
+  handleLike= () => {
+    this.setState((prevState) => ({
+      likes: prevState.likes + 1,
+      totalRatings: prevState.totalRatings + 1
+    }));
+  };
+  handleDislike= () => {
+    this.setState((prevState) => ({
+      dislikes: prevState.dislikes + 1,
+      totalRatings: prevState.totalRatings + 1
+    }));
   }
+
   render() {
     return (
      <>
@@ -32,10 +33,10 @@ class ContentRating extends Component {
             //Add text here
         </p>
         <div className='rating-buttons'>
-            <button className='like-button' onClick={this.state.handleLike}>
+            <button className='like-button' onClick={this.handleLike}>
                 Like ({this.state.likes})
             </button>
-            <button className='dislike-button' onClick={this.state.handleDislike}>
+            <button className='dislike-button' onClick={this.handleDislike}>
                 Dislike ({this.state.dislikes})
             </button>
         </div>
